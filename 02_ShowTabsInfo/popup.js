4e2e641c70d408d339749a00909a2944f4e79789
kmizuk <mizukake213@gmail.com>
2023-12-09 17:15:58 +0900

    second

--- 02_ShowTabsInfo/popup.js
+++ 02_ShowTabsInfo/popup.js
@@ -1,5 +1,13 @@
 'use strict';
 
 chrome.tabs.query({}, function(tabs) {
-
+  var i;
+  var results = document.getElementById('results');
+  var titles = [];
+  for (i = 0; i < tabs.length; i++) {
+    titles.push(tabs[i].title);
+    // console.log(tabs[i].title);
+  }
+  results.value = titles.join("\n");
+  results.select();
 });
